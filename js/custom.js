8fdbe4cf453a424df7656617db053db2ff27cf1f
Jiyeon Lim <jyeon83@gmail.com>
2017-08-03 18:00:18 +0900

    portal(main) 작업 - 중간저장

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+
+    // Tab
+    $('.tab-pane:first').show();
+    $('.tab').click(function () {
+        $('.tab').removeClass('active');
+        $(this).addClass('active');
+        $(this).parent().siblings().children('.tab-pane').hide();
+        var activeTab = $(this).attr('rel');
+        $('#' + activeTab).fadeIn();
+    });
+
+});
+
 // Select Box
 function selectboxEvent(target) {
     var $this = $(target),
