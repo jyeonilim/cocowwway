59871a01d6953ea8f1656ba878d7f9ee3ff28fa0
Jiyeon Lim <jyeon83@gmail.com>
2017-08-07 18:00:48 +0900

    portal(main) 작업 - 중간저장

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
+    $(".menu > li > a").on({
+        'mouseenter focusin': function() {
+            $(this).closest("li").siblings().find('.open').stop().stop().stop().fadeTo(50, 0, function() {
+                $(this).hide().removeClass('open');
+            });
+            $(this).siblings('.menu-child').show().addClass('open').stop().stop().fadeTo(100, 1);
+        },
+        'focusout mouseleave': function() {
+            $(this).siblings('.menu-child').fadeTo(50, 0, function() {
+                $(this).hide().removeClass('open');
+            });
+        }
+    });
+    $(document)
+        .on('mouseenter', '.menu-child.open', function(){
+            $(this).stop().stop().fadeTo(50, 1);
+        })
+        .on('mouseleave', '.menu-child.open', function(){
+            $(this).fadeTo(50, 0, function(){ $(this).hide().removeClass('open');});
+        });
+
     // Tab
     $('.tabs').each(function() {
         var $active, $content, $links = $(this).find('a');
